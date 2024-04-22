8172703cb5b5a601292ece77779e44ded6375e7f
Peter Uadiale <fxavy02@gmail.com>
2024-04-22 23:43:36 +0100

    styles: modify navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,24 +10,25 @@ import useCartSlider from "@/hooks/useCartSlider";
 import useLikedSlider from "@/hooks/useLikedSlide";
 import useCartData from "@/hooks/useCartData";
 
+import clientOnly from "./ClientOnly";
+
 import {
   AiOutlineSearch,
   AiOutlineHeart,
   AiOutlineClose,
   AiOutlineMenu,
   AiOutlineShoppingCart,
+  AiFillHome,
 } from "react-icons/ai";
-import { FaSocks } from "react-icons/fa";
-import { IoShirtSharp } from "react-icons/io5";
-import { GiPowerRing } from "react-icons/gi";
+import { FaFemale, FaMale } from "react-icons/fa";
 import { BiSolidShoppingBag, BiCertification } from "react-icons/bi";
 
 const links = [
-  { id: 1, name: "Shirts", href: "shirts", icon: IoShirtSharp },
-  { id: 2, name: "Socks", href: "socks", icon: FaSocks },
-  { id: 3, name: "Accessories", href: "accessories", icon: GiPowerRing },
-  { id: 4, name: "Shop All", href: "shop", icon: BiSolidShoppingBag },
-  { id: 5, name: "Bestsellers", href: "bestsellers", icon: BiCertification },
+  { id: 1, name: "Home", href: "/", icon: AiFillHome },
+  { id: 2, name: "Men", href: "/men", icon: FaMale },
+  { id: 3, name: "Women", href: "/women", icon: FaFemale },
+  { id: 4, name: "Shop All", href: "/shop", icon: BiSolidShoppingBag },
+  { id: 5, name: "Bestsellers", href: "/bestsellers", icon: BiCertification },
 ];
 
 const Navbar = () => {
@@ -45,8 +46,8 @@ const Navbar = () => {
   };
 
   return (
-    <header className="shadow-md sticky top-0 z-20 bg-white">
-      <div className="navbar justify-between p-4 max-w-[1440px] mx-auto">
+    <header className="sticky top-0 z-20 bg-white">
+      <div className="navbar justify-between py-4 px-12 max-md:px-4 max-w-[1440px] mx-auto">
         <div className="max-md:mx-auto">
           <Link href="/">
             <Image
@@ -59,23 +60,16 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <button
-          onClick={handleClick}
-          className="hidden max-md:block absolute top-7 left-6 z-50 text-near-black"
-        >
-          <NavToggle size={28} />
-        </button>
-
         <div
-          className={`flex justify-center z-30 items-center w-full duration-[400ms] max-md:fixed max-md:inset-y-0 max-md:right-0 max-md:left-[30%] max-md:h-full max-md:bg-white transition ${
+          className={`flex justify-start z-30 items-center w-max max-md:w-full duration-[400ms] max-md:fixed max-md:inset-y-0 max-md:right-0 max-md:left-[30%] max-md:h-full max-md:bg-white transition ${
             isOpen ? "max-md:translate-x-[0%]" : "max-md:translate-x-[100%]"
           }`}
         >
           <nav className="max-md:w-full max-md:px-6 max-md:pt-20 max-md:h-full">
-            <ul className="flex max-md:flex-col max-md:items-start max-md:w-full items-center gap-6 max-md:gap-8">
+            <ul className="flex max-md:flex-col max-md:items-start max-md:w-full items-center gap-6 lg:gap-10 max-md:gap-8">
               {links.map((link) => {
                 const Icon = link.icon;
-                const isActive = pathname === `/${link.href}`;
+                const isActive = pathname === `${link.href}`;
 
                 return (
                   <div
@@ -83,14 +77,14 @@ const Navbar = () => {
                     className="flex items-center gap-3 text-slate-600"
                     onClick={() => setIsOpen(false)}
                   >
-                    <span className="hidden max-md:block">
+                    <div className="hidden max-md:block">
                       <Icon size={18} />
-                    </span>
+                    </div>
                     <li
-                      className={`text-lg font-medium hover:text-slate-800 border-b-2 hover:border-b-slate-800 transition ${
+                      className={`text-base hover:text-main-black transition ${
                         isActive
-                          ? "border-b-2 border-b-slate-800 text-slate-800"
-                          : "border-b-transparent"
+                          ? "text-main-black font-bold"
+                          : "text-slate-500 font-medium"
                       }`}
                     >
                       <Link href={link.href}>{link.name}</Link>
@@ -102,11 +96,18 @@ const Navbar = () => {
           </nav>
         </div>
 
+        <button
+          onClick={handleClick}
+          className="hidden max-md:block absolute top-7 left-6 z-50 text-near-black"
+        >
+          <NavToggle size={28} />
+        </button>
+
         <div className="flex items-center gap-4 md:gap-6">
           <span className="cursor-pointer text-slate-600 hover:text-slate-900 active:scale-90">
             <AiOutlineSearch size={24} />
           </span>
-          <span
+          <div
             className="cursor-pointer relative text-slate-600 hover:text-slate-900 active:scale-90"
             onClick={cartSlider.onOpen}
           >
@@ -116,7 +117,7 @@ const Navbar = () => {
                 {cart?.total_items}
               </span>
             )}
-          </span>
+          </div>
           <span
             className="cursor-pointer text-slate-600 hover:text-slate-900 active:scale-90"
             onClick={likedSlider.onOpen}
@@ -129,4 +130,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default clientOnly(Navbar);
