e0ef82a713c0f5c19b3a7ea8940903e182630296
Peter Uadiale <fxavy02@gmail.com>
2024-04-23 12:15:32 +0100

    refactor: modify card component layout

--- app/(home)/_components/Card.tsx
+++ app/(home)/_components/Card.tsx
@@ -1,12 +1,14 @@
-import { Category } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-import { AiOutlineArrowRight } from "react-icons/ai";
+import Button from "@/components/Button";
+
+import { Category } from "@/types";
 
 interface CardProps {
   category: Category;
 }
+
 const Card: React.FC<CardProps> = ({ category }) => {
   const { name, slug, assets } = category;
   const [{ url }] = assets!;
@@ -14,25 +16,29 @@ const Card: React.FC<CardProps> = ({ category }) => {
   if (!category) return;
 
   return (
-    <div className="hover:-translate-y-4 transition duration-300">
-      <div className="bg-gray-300 rounded-md flex justify-center items-center h-52">
+    <div className="h-80 bg-gray-300 rounded-2xl flex group justify-center items-center relative overflow-hidden">
+      <div className="w-full h-full bg-black/10 absolute inset-0 z-10" />
+
+      <div>
+        <Image
+          src={url}
+          alt={name}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+          className="object-cover h-48 w-auto group-hover:scale-105 transition duration-300"
+        />
+      </div>
+
+      <div className="absolute bottom-4 left-4 z-20 flex flex-col justify-center gap-3">
+        <h4 className="text-white uppercase text-2xl md:text-4xl font-semibold">
+          {name}
+        </h4>
         <Link href={`/${slug}`}>
-          <Image
-            src={url}
-            alt={name}
-            width={150}
-            height={150}
-            className="object-cover h-48"
-          />
+          <Button className="bg-white text-main-black rounded-full max-w-[200px] w-full border-none capitalize hover:text-white transition">
+            See details
+          </Button>
         </Link>
       </div>
-      <Link
-        href={`/${slug}`}
-        className="flex items-center gap-3 text-slate-800 mt-4 w-fit"
-      >
-        <p className="font-semibold text-base link-hover-c">{name}</p>
-        <AiOutlineArrowRight size={18} />
-      </Link>
     </div>
   );
 };
