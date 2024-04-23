b7f6a9cb866d02efe5c9250df2e673e1a0371548
Peter Uadiale <fxavy02@gmail.com>
2024-04-23 17:47:50 +0100

    refactor: modify newsletter component styles

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,11 +1,11 @@
 const Newsletter = () => {
   return (
-    <div className="bg-gray-400 w-full min-h-[350px] grid place-items-center text-center p-4">
+    <div className="max-w-[1440px] mx-auto bg-primary w-full min-h-[350px] rounded-2xl grid place-items-center text-center p-4">
       <div className="max-w-[1440px] mx-auto">
-        <h2 className="text-3xl text-slate-900 font-semibold leading-[50px]">
+        <h2 className="text-3xl text-white font-semibold leading-[50px]">
           Let&apos;s stay in touch
         </h2>
-        <p className="text-slate-800 font-medium leading-7 mb-6 max-w-xl">
+        <p className="text-gray-200 font-medium leading-7 mb-6 max-w-xl">
           Subscribe to our newsletter for exclusive offers and updates.
         </p>
         <form id="newsletter">
@@ -13,9 +13,9 @@ const Newsletter = () => {
             <input
               type="email"
               placeholder="Your e-mail"
-              className="px-4 py-2 text-slate-700 rounded-l-lg w-full h-12 outline-none"
+              className="px-4 py-2 bg-slate-700 text-gray-100 rounded-l-lg w-full h-12 outline-none"
             />
-            <button className="bg-primary text-white text-xs md:text-sm py-2 px-2 rounded-r-lg w-fit md:w-[250px] h-12">
+            <button className="bg-white text-main-black font-semibold text-xs md:text-sm py-2 px-2 rounded-r-lg w-fit md:w-[250px] h-12">
               Get $10 Savings
             </button>
           </div>
