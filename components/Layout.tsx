d05b620053b5937c7303c8d86990a40f004eea3e
Peter Uadiale <fxavy02@gmail.com>
2024-04-25 15:50:22 +0100

    refactor: remove newsletter component from layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import Newsletter from "./Newsletter";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,9 +10,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <>
       <Navbar />
       {children}
-      <div className="px-4 md:px-8 pb-8 md:pb-14">
-        <Newsletter />
-      </div>
       <Footer />
     </>
   );
