e435fae6b0de23a4cce92b4178c2a4dc96eee748
Peter Uadiale <fxavy02@gmail.com>
2024-04-26 16:38:53 +0100

    refactor: dynamically import ProductCard component

--- app/(home)/_components/PageContent.tsx
+++ app/(home)/_components/PageContent.tsx
@@ -1,32 +1,43 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { motion } from "framer-motion";
 import { SwiperSlide } from "swiper/react";
 
 import { Product } from "@chec/commerce.js/types/product";
+import { Category } from "@chec/commerce.js/types/category";
+import { Cart } from "@chec/commerce.js/types/cart";
 
-import ProductCard from "@/components/ProductCard";
 import SlideContainer from "@/components/SlideContainer";
 import Header from "@/components/Header";
 import Newsletter from "@/components/Newsletter";
-
+import clientOnly from "@/components/ClientOnly";
 import Card from "./Card";
-import { Category } from "@chec/commerce.js/types/category";
+
 import { categoryCardContainerVariants } from "@/utils/animations";
 
 type PageContentProps = {
   products: Product[];
   categories: Category[];
+  cart: Cart;
 };
 
-const PageContent: React.FC<PageContentProps> = ({ products, categories }) => {
+const ProductCard = dynamic(() => import("@/components/ProductCard"), {
+  loading: () => <div className="skeleton h-80 w-full rounded-2xl"></div>,
+});
+
+const PageContent: React.FC<PageContentProps> = ({
+  products,
+  categories,
+  cart,
+}) => {
   return (
     <>
       <section>
         <SlideContainer title="Featured Products">
           {products.map((product) => (
             <SwiperSlide key={product.id} className="w-full">
-              <ProductCard product={product} />
+              <ProductCard product={product} cart={cart} />
             </SwiperSlide>
           ))}
         </SlideContainer>
@@ -53,7 +64,7 @@ const PageContent: React.FC<PageContentProps> = ({ products, categories }) => {
         <Header title="Best-selling Products" />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
           {products.slice(0, 8).map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard key={product.id} product={product} cart={cart} />
           ))}
         </div>
       </section>
@@ -65,4 +76,4 @@ const PageContent: React.FC<PageContentProps> = ({ products, categories }) => {
   );
 };
 
-export default PageContent;
+export default clientOnly(PageContent);
