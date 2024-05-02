d32cf292739d537e3c5739835e931ef8318e9e16
Peter Uadiale <fxavy02@gmail.com>
2024-05-02 18:59:19 +0100

    feat: create signup page

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+import PageComponent from "./_components/PageComponent";
+
+export default async function LoginPage() {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-12 px-4 md:px-8 lg:px-12 max-w-[1440px] mx-auto mb-14">
+      <div className="order-2 md:order-1">
+        <Image
+          src="/signup.svg"
+          width={500}
+          height={500}
+          alt="login illustration"
+        />
+      </div>
+
+      <PageComponent />
+    </section>
+  );
+}
