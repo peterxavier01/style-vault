f5943f59b289bb4f253b75f89da81d3d8272806c
Peter Uadiale <fxavy02@gmail.com>
2024-05-02 19:00:42 +0100

    refactor: pass user auth data as props to navbar component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,7 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -5,10 +9,15 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = async ({ children }) => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data } = await supabase.auth.getUser();
+
   return (
     <>
-      <Navbar />
+      <Navbar data={data} />
       {children}
       <Footer />
     </>
