2d827b8f3d65c7d2527ab1c14ef00bb82bac99be
Peter Uadiale <fxavy02@gmail.com>
2024-05-04 14:12:37 +0100

    refactor: modify commerce.ts import type

--- utils/commerce.ts
+++ utils/commerce.ts
@@ -1,5 +1,5 @@
 import CommerceSDK from "@chec/commerce.js";
 
-const commerce = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY!);
+const commerce = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY as string);
 
 export default commerce;
