86ceb7bdffb5a355304e3a99558a2608e9099adb
Peter Uadiale <fxavy02@gmail.com>
2024-05-12 01:06:48 +0100

    reafctor: update logic to initialize commerce.js

--- utils/commerce.ts
+++ utils/commerce.ts
@@ -1,13 +1,5 @@
 import CommerceSDK from "@chec/commerce.js";
 
-let commerce: CommerceSDK;
+const client = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_API_KEY as string);
 
-try {
-  const publicKey = process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY as string;
-  commerce = new CommerceSDK(publicKey);
-} catch (error) {
-  console.error("Error initializing Commerce.js:", error);
-  throw new Error("Failed to initialize Commerce.js");
-}
-
-export default commerce;
+export default client;
