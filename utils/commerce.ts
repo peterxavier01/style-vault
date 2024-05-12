cab1385bf0c2665ba9198e901f813743835a89e3
Peter Uadiale <fxavy02@gmail.com>
2024-05-12 01:27:19 +0100

    refactor: modify commerce.js initialization logic

--- utils/commerce.ts
+++ utils/commerce.ts
@@ -1,5 +1,7 @@
 import CommerceSDK from "@chec/commerce.js";
 
-const client = new CommerceSDK(process.env.NEXT_PUBLIC_CHEC_API_KEY as string);
+const client = new CommerceSDK(
+  encodeURIComponent((process.env.NEXT_PUBLIC_CHEC_API_KEY || "").trim())
+);
 
 export default client;
