c5307df0f0e2f8f3acb78f0ae4e68dd98d807937
Peter Uadiale <fxavy02@gmail.com>
2024-05-28 19:58:55 +0100

    refactor: add new prop - checked to show default checked dropdown

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -3,13 +3,14 @@ import { ProductCategory } from "@/types";
 type DropdownProps = {
   title: string;
   content: ProductCategory[];
+  checked?: boolean;
   onClick: (slug: string) => void;
 };
 
-const Dropdown = ({ title, content, onClick }: DropdownProps) => {
+const Dropdown = ({ title, content, checked, onClick }: DropdownProps) => {
   return (
-    <div className="collapse collapse-arrow bg-primary">
-      <input type="checkbox" />
+    <div className="collapse collapse-arrow bg-primary h-fit">
+      <input type="checkbox" defaultChecked={checked} />
       <div className="collapse-title text-sm sm:text-lg text-white capitalize font-medium">
         {title}
       </div>
