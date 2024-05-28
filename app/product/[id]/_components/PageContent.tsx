0b764173b2641eb63315523fc3831fb30feaa07d
Peter Uadiale <fxavy02@gmail.com>
2024-05-28 01:46:23 +0100

    refactor: add similar products component to page layout

--- app/product/[id]/_components/PageContent.tsx
+++ app/product/[id]/_components/PageContent.tsx
@@ -5,17 +5,18 @@ import { useEffect, useState } from "react";
 
 import sanitizeHtml from "sanitize-html";
 import { Product } from "@chec/commerce.js/types/product";
+import { LineItem } from "@chec/commerce.js/types/line-item";
 import { MdOutlineShoppingBag } from "react-icons/md";
 
 import Button from "@/components/Button";
 import Color from "@/components/Color";
 import Size from "@/components/Size";
 import Counter from "@/components/Counter";
+import SimilarProduct from "@/components/SimilarProduct";
 import clientOnly from "@/components/ClientOnly";
 
 import addToCart from "@/libs/addToCart";
 import useCartData from "@/hooks/useCartData";
-import { LineItem } from "@chec/commerce.js/types/line-item";
 
 type PageComponentProps = {
   product: Product;
@@ -51,83 +52,89 @@ const PageContent = ({ product }: PageComponentProps) => {
   if (!product) return;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-20">
-      <div className="flex flex-col gap-4">
-        <div className="temporary w-full min-h-[350px] md:min-h-[400px] bg-gray-300 rounded-xl relative">
-          <Image
-            src={selectedImage as string}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            priority
-            className="w-full block rounded-2xl object-cover"
-            alt="image-name"
-          />
-        </div>
-        <div className="flex items-center gap-4 w-fit rounded-xl">
-          {product.assets.map((item) => (
-            <div
-              key={item.id}
-              className="relative overflow-hidden flex items-center justify-center w-[150px] h-[140px] bg-gray-300 rounded-xl"
-            >
-              <Image
-                src={item.url}
-                width={item.image_dimensions.width}
-                height={item.image_dimensions.height}
-                alt="image-name"
-                className="rounded-2xl object-contain bg-gray-300 block"
-                onMouseEnter={() => handleImageSelected(item.url)}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="">
-        <p className="text-slate-500 text-base font-medium mb-2">
-          {product.categories[0].name}
-        </p>
-        <h1 className="text-3xl md:text-5xl font-bold text-main-black mb-4">
-          {product.name}
-        </h1>
-        <p
-          dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-          className="text-slate-500 text-sm mb-8"
-        />
-
-        <Color colors={product.variant_groups[0]} />
-
-        <Size sizes={product.variant_groups[1]} />
-
-        <div className="flex items-center justify-between mb-8">
-          <div>
-            <p className="text-main-black font-medium capitalize mb-2">
-              total price
-            </p>
-            <p className="text-3xl text-main-black font-semibold">
-              {product.price.formatted_with_symbol}
-            </p>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-20">
+        <div className="flex flex-col gap-4">
+          <div className="temporary w-full min-h-[350px] md:min-h-[400px] bg-gray-300 rounded-xl relative">
+            <Image
+              src={selectedImage as string}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              priority
+              className="w-full block rounded-2xl object-cover"
+              alt="image-name"
+            />
           </div>
-          {cartItem.quantity && (
-            <div>
-              <Counter
-                quantity={cartItem.quantity as number}
-                cartItem={cartItem as LineItem}
-              />
-            </div>
-          )}
+          <div className="flex items-center gap-4 w-fit rounded-xl">
+            {product.assets.map((item) => (
+              <div
+                key={item.id}
+                className="relative overflow-hidden flex items-center justify-center w-[150px] h-[140px] bg-gray-300 rounded-xl"
+              >
+                <Image
+                  src={item.url}
+                  fill
+                  sizes="150px"
+                  alt="image-name"
+                  className="rounded-2xl object-cover bg-gray-300 block"
+                  onMouseEnter={() => handleImageSelected(item.url)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div className="flex items-center gap-4">
-          <Button
-            className="rounded-xl capitalize"
-            onClick={() => addToCart(product.id, 1)}
-          >
-            <MdOutlineShoppingBag size={24} />
-            Add to Cart
-          </Button>
+        <div>
+          <p className="text-slate-500 text-base font-medium mb-2">
+            {product.categories[0].name}
+          </p>
+          <h1 className="text-3xl md:text-5xl font-bold text-main-black mb-4">
+            {product.name}
+          </h1>
+          <p
+            dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+            className="text-slate-500 text-sm mb-8"
+          />
+
+          <Color colors={product.variant_groups[0]} />
+
+          <Size sizes={product.variant_groups[1]} />
+
+          <div className="flex items-center justify-between mb-8">
+            <div>
+              <p className="text-main-black font-medium capitalize mb-2">
+                total price
+              </p>
+              <p className="text-3xl text-main-black font-semibold">
+                {product.price.formatted_with_symbol}
+              </p>
+            </div>
+            {cartItem.quantity && (
+              <div>
+                <Counter
+                  quantity={cartItem.quantity as number}
+                  cartItem={cartItem as LineItem}
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="flex items-center gap-4">
+            <Button
+              className="rounded-xl capitalize"
+              onClick={() => addToCart(product.id, 1)}
+            >
+              <MdOutlineShoppingBag size={24} />
+              Add to Cart
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {product.related_products.length > 0 ? (
+        <SimilarProduct product={product} />
+      ) : null}
+    </>
   );
 };
 
