c0f253fc35af53b83a8309a598736cfdce583ccd
Peter Uadiale <fxavy02@gmail.com>
2024-05-28 01:42:33 +0100

    feat: 📝create checkout button component, 🎉add confetti animation, 💰add logic to make payment with paystack

---
+++ app/checkout/_components/CheckoutButton.tsx
@@ -0,0 +1,59 @@
+import { usePaystackPayment } from "react-paystack";
+import { PaystackProps, callback } from "react-paystack/dist/types";
+
+import useCheckoutData from "@/hooks/useCheckoutData";
+
+import Button from "@/components/Button";
+import { IFormData } from "./CheckoutForm";
+import { refreshCart } from "@/libs/updateCart";
+import { convertToSubunit } from "@/utils";
+
+interface CheckoutButtonProps {
+  checkoutData: IFormData | undefined;
+  activeStep: number;
+  setActiveStep: (step: number) => void;
+}
+
+const CheckoutButton = ({
+  checkoutData,
+  activeStep,
+  setActiveStep,
+}: CheckoutButtonProps) => {
+  const checkoutLiveObject = useCheckoutData(
+    (state) => state.checkoutLiveObject
+  );
+
+  const config: PaystackProps | undefined = {
+    reference: new Date().getTime().toString(),
+    email: checkoutData?.email || "",
+    amount: convertToSubunit(
+      checkoutLiveObject?.total.raw || 0,
+      checkoutLiveObject?.currency.code.toUpperCase() || ""
+    ),
+    publicKey: process.env.NEXT_PUBLIC_PAYSTACK_API_KEY as string,
+  };
+
+  async function onCheckoutComplete() {
+    await refreshCart();
+  }
+
+  const onSuccess: callback | undefined = (reference: any) => {
+    console.log(reference);
+    setActiveStep(activeStep + 1);
+    onCheckoutComplete();
+  };
+
+  const initializePayment = usePaystackPayment(config);
+
+  return (
+    <Button
+      className="w-full max-w-xs focus-visible:outline focus-visible:outline-primary"
+      type="submit"
+      onClick={() => initializePayment({ onSuccess })}
+    >
+      Confirm Order
+    </Button>
+  );
+};
+
+export default CheckoutButton;
