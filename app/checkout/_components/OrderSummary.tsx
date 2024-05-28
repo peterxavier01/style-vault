1f2a107483d6cb4873f76410a7e0b222c781c770
Peter Uadiale <fxavy02@gmail.com>
2024-05-28 01:44:52 +0100

    feat: create order summary component, add logic to fetch live checkout object

---
+++ app/checkout/_components/OrderSummary.tsx
@@ -0,0 +1,101 @@
+import { SetStateAction, useEffect } from "react";
+
+import { CheckoutToken } from "@chec/commerce.js/types/checkout-token";
+
+import commerce from "@/utils/commerce";
+import CartItem from "@/components/CartItem";
+import useCheckoutData from "@/hooks/useCheckoutData";
+import CheckoutButton from "./CheckoutButton";
+import Button from "@/components/Button";
+
+interface OrderSummaryProps {
+  checkoutToken: CheckoutToken | null;
+  activeStep: number;
+  setActiveStep: React.Dispatch<SetStateAction<number>>;
+}
+
+const OrderSummary = ({
+  checkoutToken,
+  activeStep,
+  setActiveStep,
+}: OrderSummaryProps) => {
+  const checkoutLiveObject = useCheckoutData(
+    (state) => state.checkoutLiveObject
+  );
+  const checkoutData = useCheckoutData((state) => state.checkoutData);
+
+  useEffect(() => {
+    const setCheckoutLiveObject =
+      useCheckoutData.getState().setCheckoutLiveObject;
+
+    const getLiveObject = async (tokenId: string) => {
+      const object = await commerce.checkout.getLive(tokenId);
+      setCheckoutLiveObject(object);
+    };
+
+    if (checkoutToken) {
+      getLiveObject(checkoutToken?.id);
+    }
+  }, [checkoutToken]);
+
+  return (
+    <>
+      {checkoutLiveObject && checkoutLiveObject?.line_items.length > 0 ? (
+        <section className="col-span-1 h-full max-w-xl mx-auto">
+          <div className="bg-white rounded-md px-4 py-8 flex flex-col gap-6 h-full">
+            {checkoutLiveObject?.line_items.map((product) => (
+              <CartItem
+                key={product.id}
+                cartItem={product}
+                showCloseBtn={false}
+                onRemoveItem={() => {}}
+              />
+            ))}
+
+            <div className="mt-auto">
+              <div className="border-y border-slate-300 py-2">
+                <p className="flex items-center justify-between">
+                  <span>Subtotal</span>
+                  <span>
+                    {checkoutLiveObject?.subtotal.formatted_with_symbol}
+                  </span>
+                </p>
+                <p className="flex items-center justify-between mt-4">
+                  <span>Shipping</span>
+                  <span>
+                    {checkoutLiveObject?.shipping?.price
+                      ?.formatted_with_symbol || "-"}
+                  </span>
+                </p>
+              </div>
+
+              <p className="flex items-center justify-between mt-4">
+                <span>
+                  Total ({checkoutLiveObject.currency.code.toUpperCase()})
+                </span>
+                <span>{checkoutLiveObject.subtotal.formatted_with_symbol}</span>
+              </p>
+            </div>
+
+            <div className="flex items-center flex-wrap sm:flex-nowrap w-full justify-between gap-4">
+              <Button
+                onMouseDown={() => setActiveStep(activeStep - 1)}
+                className="w-full sm:max-w-[200px]"
+              >
+                Prev
+              </Button>
+
+              <CheckoutButton
+                checkoutData={checkoutData}
+                activeStep={activeStep}
+                setActiveStep={setActiveStep}
+              />
+            </div>
+          </div>
+        </section>
+      ) : null}
+    </>
+  );
+};
+
+export default OrderSummary;
