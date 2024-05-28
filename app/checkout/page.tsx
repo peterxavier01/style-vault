b3ec455c45b3e33fdebb072910b5c913f3df86bb
Peter Uadiale <fxavy02@gmail.com>
2024-05-28 01:40:14 +0100

    feat: create checkout route

---
+++ app/checkout/page.tsx
@@ -0,0 +1,11 @@
+import PageComponent from "./_components/PageComponent";
+
+const CheckoutPage = () => {
+  return (
+    <section className="max-w-[1440px] mx-auto px-4 md:px-8 bg-gray-100">
+      <PageComponent />
+    </section>
+  );
+};
+
+export default CheckoutPage;
