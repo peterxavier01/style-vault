303c4d1edccb8e8bd2641a1787e8f696c7695495
Peter Uadiale <fxavy02@gmail.com>
2024-05-30 10:54:48 +0100

    feat: add logic to query products

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,14 +1,48 @@
-import { useRef } from "react";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import debounce from "lodash.debounce";
+import { Product } from "@chec/commerce.js/types/product";
+
+import ProductSearchCard from "./ProductSearchCard";
+
+import getProducts from "@/libs/getProducts";
 
 const Search = () => {
   const modalRef = useRef<HTMLDialogElement | null>(null);
+  const [products, setProducts] = useState<Product[] | null>(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState<
+    Product[] | undefined
+  >(undefined);
 
   const openModal = () => {
     if (modalRef.current) {
       modalRef.current.showModal();
     }
   };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products: Product[] = await getProducts();
+      setProducts(products);
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+    debouncedProductFilter();
+  };
+
+  const debouncedProductFilter = debounce(() => {
+    const filteredProducts = products?.filter((product) =>
+      product.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredProducts(filteredProducts);
+  }, 500);
 
   return (
     <>
@@ -21,17 +55,31 @@ const Search = () => {
 
       <dialog className="modal" ref={modalRef}>
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/** if there is a button in form, it will close the modal **/}
-              <button className="btn">Close</button>
-            </form>
-          </div>
+          <input
+            type="text"
+            placeholder="Search products..."
+            value={searchQuery}
+            onChange={handleSearch}
+            className="w-full input bg-gray-100 rounded-xl text-slate-800 mb-6 py-8 placeholder:text-slate-600"
+          />
+
+          <ul>
+            {searchQuery.length > 0
+              ? filteredProducts?.map((product) => (
+                  <ProductSearchCard
+                    key={product.id}
+                    product={product}
+                    modalRef={modalRef}
+                    setSearchQuery={setSearchQuery}
+                  />
+                ))
+              : null}
+          </ul>
         </div>
+
+        <form method="dialog" className="modal-backdrop">
+          <button>Close</button>
+        </form>
       </dialog>
     </>
   );
