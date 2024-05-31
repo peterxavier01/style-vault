0f8f52a4d1ad7b61b54d0d9ad9cade4a1212283b
Peter Uadiale <fxavy02@gmail.com>
2024-06-01 00:38:10 +0100

    feat: add the theme toggle component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 import useCartSlider from "@/hooks/useCartSlider";
 import useCartData from "@/hooks/useCartData";
 import getCart from "@/libs/getCart";
 
 import clientOnly from "./ClientOnly";
+import Search from "./Search";
+import ThemeToggle from "./ThemeToggle";
 
 import {
   AiOutlineClose,
@@ -19,7 +22,6 @@ import {
 } from "react-icons/ai";
 import { FaFemale, FaMale } from "react-icons/fa";
 import { BiSolidShoppingBag } from "react-icons/bi";
-import Search from "./Search";
 
 const links = [
   { id: 1, name: "Home", href: "/", icon: AiFillHome },
@@ -30,6 +32,7 @@ const links = [
 
 const Navbar = () => {
   const { cart, setCart } = useCartData();
+  const { theme } = useTheme();
 
   useEffect(() => {
     const getCartData = async () => {
@@ -56,12 +59,12 @@ const Navbar = () => {
   };
 
   return (
-    <header className="sticky top-0 z-50 bg-white">
+    <header className="sticky top-0 z-50 bg-white dark:bg-dark-primary">
       <div className="navbar justify-between py-4 px-12 max-md:px-4 max-w-[1440px] mx-auto">
         <div className="max-md:mx-auto">
           <Link href="/">
             <Image
-              src="/logo-dark.png"
+              src={theme === "light" ? "/logo-dark.png" : "/logo-light.png"}
               alt="style vault logo"
               width={100}
               height={100}
@@ -71,7 +74,7 @@ const Navbar = () => {
         </div>
 
         <div
-          className={`flex justify-start z-30 items-center w-max max-md:w-full duration-[400ms] max-md:fixed max-md:inset-y-0 max-md:right-0 max-md:left-[30%] max-md:h-full max-md:bg-white transition ${
+          className={`flex justify-start z-30 items-center w-max max-md:w-full duration-[400ms] max-md:fixed max-md:inset-y-0 max-md:right-0 max-md:left-[30%] max-md:h-full max-md:bg-white max-md:dark:bg-dark-secondary transition ${
             isOpen ? "max-md:translate-x-[0%]" : "max-md:translate-x-[100%]"
           }`}
         >
@@ -84,17 +87,24 @@ const Navbar = () => {
                 return (
                   <div
                     key={link.id}
-                    className="flex items-center gap-3 text-slate-600"
+                    className="flex items-center gap-3 text-slate-600 dark:text-gray-300"
                     onClick={() => setIsOpen(false)}
                   >
                     <div className="hidden max-md:block">
-                      <Icon size={22} />
+                      <Icon
+                        size={22}
+                        className={
+                          isActive
+                            ? "text-main-black dark:text-white"
+                            : "text-slate-500"
+                        }
+                      />
                     </div>
                     <li
-                      className={`text-lg md:text-base hover:text-main-black transition ${
+                      className={`text-lg md:text-base hover:text-main-black hover:dark:text-white transition ${
                         isActive
-                          ? "text-main-black font-bold"
-                          : "text-slate-500 font-medium"
+                          ? "text-main-black dark:text-white font-bold"
+                          : "text-slate-500 dark:text-gray-300 font-medium"
                       }`}
                     >
                       <Link href={link.href}>{link.name}</Link>
@@ -108,15 +118,16 @@ const Navbar = () => {
 
         <button
           onClick={handleClick}
-          className="hidden max-md:block absolute top-7 left-6 z-50 text-near-black"
+          className="hidden max-md:block absolute top-7 left-6 z-50"
         >
-          <NavToggle size={28} />
+          <NavToggle size={28} className="text-slate-800 dark:text-gray-300" />
         </button>
 
         <div className="flex items-center gap-4 md:gap-6">
           <Search />
+
           <div
-            className="cursor-pointer relative text-slate-600 hover:text-slate-900 active:scale-90"
+            className="cursor-pointer relative text-slate-600 dark:text-gray-300 hover:text-slate-900 hover:dark:text-white active:scale-90"
             onClick={cartSlider.onOpen}
           >
             <AiOutlineShoppingCart size={24} />
@@ -126,6 +137,8 @@ const Navbar = () => {
               </span>
             )}
           </div>
+
+          <ThemeToggle />
         </div>
       </div>
     </header>
