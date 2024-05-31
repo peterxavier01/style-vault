96850697b0d3dc51e203bda21ef0540aaa4793d0
Peter Uadiale <fxavy02@gmail.com>
2024-06-01 00:32:17 +0100

    refactor: configure tailwind to toggle theme manually

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +12,8 @@ const config: Config = {
       colors: {
         primary: "#03343d",
         "main-black": "#040308",
+        "dark-primary": "#121212",
+        "dark-secondary": "#262626",
       },
       gridTemplateColumns: {
         products: "repeat(auto-fit, minmax(200px, 1fr))",
