30cbcda671b4e5231e780f6fdbf779078dd57afe
Peter Uadiale <fxavy02@gmail.com>
2024-06-01 00:41:24 +0100

    fix: modify stagger animation for category cards

--- utils/animations.ts
+++ utils/animations.ts
@@ -56,15 +56,12 @@ export const headerVariants = {
 export const categoryCardContainerVariants = {
   initial: {
     opacity: 0,
-    y: 20,
   },
   visible: {
     opacity: 1,
-    y: 0,
     transition: {
-      duration: 0.8,
-      staggerChildren: 0.5,
-      when: "beforeChildren",
+      duration: 0.3,
+      staggerChildren: 0.25,
     },
   },
 };
@@ -72,8 +69,10 @@ export const categoryCardContainerVariants = {
 export const categoryCardItemVariants = {
   initial: {
     opacity: 0,
+    y: 10,
   },
   visible: {
     opacity: 1,
+    y: 0,
   },
 };
