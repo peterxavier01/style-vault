6f6d045f952ff538c684eb273822c518a530a307
Peter Uadiale <fxavy02@gmail.com>
2024-06-01 00:40:32 +0100

    feat: create the theme toggle component to switch app theme

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,31 @@
+import { useTheme } from "next-themes";
+import { FiMoon, FiSun } from "react-icons/fi";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  const Icon = theme === "dark" ? FiMoon : FiSun;
+
+  return (
+    <div className="dropdown dropdown-end dropdown-bottom">
+      <div tabIndex={0} role="button">
+        <Icon className="text-slate-800 dark:text-gray-300" size={24} />
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-white text-slate-800 dark:bg-dark-secondary dark:text-gray-300 rounded-box w-52"
+      >
+        <li onClick={() => setTheme("light")}>
+          <a>Light</a>
+        </li>
+        <li onClick={() => setTheme("dark")}>
+          <a>Dark</a>
+        </li>
+        {/* <li onClick={() => setTheme("system")}>
+          <a>System</a>
+        </li> */}
+      </ul>
+    </div>
+  );
+};
+
+export default ThemeToggle;
