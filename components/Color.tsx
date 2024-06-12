926f0e0380c90c33627458e30a80ab8f77fb5698
Peter Uadiale <fxavy02@gmail.com>
2024-06-12 23:07:57 +0100

    refactor: remove redundant conditional rendering logic

--- components/Color.tsx
+++ components/Color.tsx
@@ -17,23 +17,24 @@ const Color = ({ colors }: ColorProps) => {
     <>
       {colors ? (
         <div className="mb-8">
-          <p className="text-main-black dark:text-gray-300 font-medium capitalize">Color</p>
+          <p className="text-main-black dark:text-gray-300 font-medium capitalize">
+            Color
+          </p>
           <div className="flex items-center gap-2 mt-2">
-            {colors &&
-              colors.options.map((color, index) => (
-                <span
-                  title={color.name}
-                  key={color.id}
-                  style={{
-                    backgroundColor: removeHyphen(color.name.toLowerCase()),
-                  }}
-                  className={cn(
-                    "w-4 h-4 rounded-full cursor-pointer block relative border border-gray-500 dark:border-gray-300",
-                    selectedColor === index ? "color-selected" : ""
-                  )}
-                  onClick={() => handleSelectedColor(index)}
-                />
-              ))}
+            {colors.options.map((color, index) => (
+              <span
+                title={color.name}
+                key={color.id}
+                style={{
+                  backgroundColor: removeHyphen(color.name.toLowerCase()),
+                }}
+                className={cn(
+                  "w-4 h-4 rounded-full cursor-pointer block relative border border-gray-500 dark:border-gray-300",
+                  selectedColor === index ? "color-selected" : ""
+                )}
+                onClick={() => handleSelectedColor(index)}
+              />
+            ))}
           </div>
         </div>
       ) : null}
--- components/Size.tsx
+++ components/Size.tsx
@@ -18,21 +18,20 @@ const Size = ({ sizes }: SizeProps) => {
             size
           </p>
           <div className="flex items-center gap-2 mt-2 relative">
-            {sizes &&
-              sizes.options.map((size, index) => (
-                <span
-                  key={size.id}
-                  className={cn(
-                    "uppercase w-full max-w-[45px] cursor-pointer h-10 rounded-xl text-sm flex items-center justify-center text-main-black dark:text-gray-300 hover:text-white hover:bg-primary transition border border-main-black dark:hover:border-primary",
-                    selectedSize === index
-                      ? "bg-primary border-primary text-white"
-                      : "dark:border-gray-300"
-                  )}
-                  onClick={() => setSelectedSize(index)}
-                >
-                  {size.name}
-                </span>
-              ))}
+            {sizes.options.map((size, index) => (
+              <span
+                key={size.id}
+                className={cn(
+                  "uppercase w-full max-w-[45px] cursor-pointer h-10 rounded-xl text-sm flex items-center justify-center text-main-black dark:text-gray-300 hover:text-white hover:bg-primary transition border border-main-black dark:hover:border-primary",
+                  selectedSize === index
+                    ? "bg-primary border-primary text-white"
+                    : "dark:border-gray-300"
+                )}
+                onClick={() => setSelectedSize(index)}
+              >
+                {size.name}
+              </span>
+            ))}
           </div>
         </div>
       ) : null}
