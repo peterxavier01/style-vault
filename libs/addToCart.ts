14712e753c6ea623f80899a5e0459d02ac826a5a
Peter Uadiale <fxavy02@gmail.com>
2024-06-12 19:23:19 +0100

    refactor: modify add to cart logic

--- libs/addToCart.ts
+++ libs/addToCart.ts
@@ -1,18 +1,14 @@
 import toast from "react-hot-toast";
 
 import commerce from "@/utils/commerce";
-import useCartData from "@/hooks/useCartData";
 
 const addToCart = async (productId: string, quantity: number) => {
-  const setCart = useCartData.getState().setCart;
-
   try {
     const { cart } = await commerce.cart.add(productId, quantity);
-    setCart(cart);
     toast.success("Product added to cart successfully");
+    return cart;
   } catch (error) {
     toast.error("Error adding product to cart");
   }
 };
-
 export default addToCart;
