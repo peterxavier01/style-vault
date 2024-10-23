c92b33f5b8a00bc0fabeb1396a2cee5775e81fbb
peteuadiale1 <peteuadiale1@gmail.com>
2024-10-23 16:16:01 +0000

    refactor: update config to use remote patterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.chec.io", "imgur.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: '',
+        pathname: "/images/q06goj7s/production/**"
+      }
+    ]
   },
 };
 
