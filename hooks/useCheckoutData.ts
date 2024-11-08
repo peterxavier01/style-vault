198b160d6cdc3c279ea6c7e0e89d4a133054e57a
Peter Uadiale <fxavy02@gmail.com>
2024-11-08 17:32:36 +0000

    refactor: remove unused import

--- hooks/useCheckoutData.ts
+++ hooks/useCheckoutData.ts
@@ -1,19 +1,13 @@
 import { create } from "zustand";
 
-import { Live } from "@chec/commerce.js/types/live";
 import { IFormData } from "@/app/checkout/_components/CheckoutForm";
 
 interface CheckoutDataStore {
-  checkoutLiveObject: Live | null;
-  setCheckoutLiveObject: (checkoutLiveObject: Live | null) => void;
   checkoutData: IFormData | undefined;
   setCheckoutData: (checkoutData: IFormData | undefined) => void;
 }
 
 const useCheckoutData = create<CheckoutDataStore>((set) => ({
-  checkoutLiveObject: null,
-  setCheckoutLiveObject: (liveObject) =>
-    set({ checkoutLiveObject: liveObject }),
   checkoutData: undefined,
   setCheckoutData: (data) => set({ checkoutData: data }),
 }));
